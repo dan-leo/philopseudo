02bea9490938ec1457d26514c5a1a447c289c63e
Daniel Robinson <d7robin@gmail.com>
2017-03-10 13:30:33 +0200

    adding Rossouw

---
+++ consquences.js
@@ -0,0 +1,34 @@
+class Consequences
+{
+
+private:
+
+public:
+
+	Consequences(){
+		
+	}
+
+	combined_consequences(){
+		return {
+			Science + Technology += "Mechanization and Urbanization"
+			Technology + "environment" += "people forced to adapt"
+		}
+	}
+
+	scientific_consequences(){
+		return {
+
+		}
+	}
+
+	technological_consequences(){
+		while (tech_progress){
+			tech_progress += tech_progress;
+		}
+		return {
+			tech_progress
+		}
+	}
+
+}
--- technology.js
+++ technology.js
@@ -2,9 +2,10 @@ class Technology
 {
 private:
 
-	_technology;
 	visible_tech = functional;
 	!visible_tech = !functional;
+
+	value_neutral = "presupposing the acceptance of any particular values"
 
 public:
 
@@ -16,7 +17,73 @@ public:
 		return _technology;
 	}
 
+	hardware(){
+		devices = {
+			tools,
+			machines,
+			instruments,
+			structures
+		}
 
+		_technology = Technology(devices);
+
+		_reflections = {
+			"Devices": _technology.manipulate(humans),
+			"Humans give tools purpose",
+			"Technology is": !value_neutral
+		}
+
+		_problems = {
+			"earliest machine was the organization of people",
+			"tech hardware does not always function",
+			"sometimes it is extensions of who we are"
+		}
+
+		return {
+			_technology, _reflections, _problems
+		}
+	}
+
+	rules(){
+		_reflections = {
+			"means-end relationships",
+			"not hardware but pattern of rules related to problem-solving?",
+			"tech as software, not hardware?",
+		}
+
+		_examples = {
+			"speech recognition",
+			"traffic control",
+			"software engineering"
+		}
+
+		_problems = {
+			"AI",
+			"tech uses reliable physical aspect of nature"
+		}
+
+		return {
+			_reflections, _examples, _problems
+		}
+	}
+
+	applied_science(){
+		_reflections = {
+			"application of science in service of the needs of humanity",
+			"some aspects of applied science is technology",
+			"tech seems value_neutral, which is misleading",
+			"historically, most of the rise in tech did not come from the knowledge of science"
+		}
+
+		_contradictions = {
+			"Thomas Edison did not know the emag theory of Maxwell",
+			"Chemical discoveries = accidents" : [
+				"pace-maker",
+				"post-it",
+				"slinky"
+			]
+		}
+	}
 
 
 };
